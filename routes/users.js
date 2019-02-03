ba7e54c600789e158a783d1af06718af3090def5
Soph1ia <37844553+Soph1ia@users.noreply.github.com>
2019-02-03 08:16:57 +0000

    UPDATING OVERALL CODE updated nodeJs and fixed some isses

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 /*Renders register page*/
-router.get('/register', function(req, res, next){
-	res.render('register');
-	});
+router.get('/register', function (req, res, next) {
+    res.render('register');
+});
 /*Renders login page*/
-router.get('/login', function(req, res, next){
-	res.render('login');
-	});
+router.get('/login', function (req, res, next) {
+    res.render('login');
+});
 /*Renders privacy policy page*/
-router.get('/policy', function(req, res, next){
-	res.render('policy');
-	});
+router.get('/policy', function (req, res, next) {
+    res.render('policy');
+});
+router.get('/interfacePage', function (req, res, next) {
+    res.render('interfacePage');
+});
 module.exports = router;
