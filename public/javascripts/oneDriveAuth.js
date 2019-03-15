b71be83ccbf6070327276a3a85a6713aae990ea0
Johnathan Nicholas <Jtnicholas2014@gmail.com>
2019-03-15 23:45:31 +0000

    Added loading screen .gif when waiting on server

--- public/javascripts/oneDriveAuth.js
+++ public/javascripts/oneDriveAuth.js
@@ -3,45 +3,60 @@ var oneDriveUsers;
 
 let uid;
 $(document).ready(() => {
-
+    //jQuery(".loading").hide();
 
     // Check if user is logged in to OneDrive
     firebase.auth().onAuthStateChanged((user) => {
+        //jQuery(".loading").show();
         if (user) {
-            //console.log("USER ", user);
             uid = user.uid;
-            //console.log("UID ", uid)
 
             //Make sure there's a valid token for user, signs out user if he/she has token and it has expired.
-            checkUserToken(uid)
+            //Welcome to callback hell.
+            hasToken(uid).then((result) => {                              // Returns a promise
+                if (result === true) {
 
-            // If redirected back to interface page from oneDrive auth, get token
-            let url = window.location.toString();
-            let index = url.indexOf("#");
-
-            if (index >= 0) {
-                let token = parseToken(url, index);
-                console.log("UID : ", uid);
-                $.ajax({
-                    type: "POST",
-                    url: "/auth/onedrive/sign-in",
-                    data: { token, uid },
-                    dataType: "json",
-                    success: function (response) {
-                        if (response.value === true) {
-                            console.log("Signed OneDrive user in...")
-                            //window.location = "/users/interfacePage";
-                            userLoggedIn();
+                    userLoggedIn();                                 // Sets OD button text and link to log out
+                    hasTokenExpired(uid).then((result) => {
+                        if (result === false) {             //Token was still valid
+                            loadPage();
+                            jQuery(".loading").hide();
                         }
-                        else {
-                            console.log("Failed to sign OneDrive user in...");
-                        }
-                    },
-                    error: (err) => {
+                    }).catch((err) => console.error("Error in hasTokenExpired(uid) method.", err));
+                }
+                else {
+                    //User does not have a token 
+                    userLoggedOut();
+                    // If redirected back to interface page from oneDrive auth, get token
+                    let url = window.location.toString();
+                    let index = url.indexOf("#");
 
+                    if (index >= 0) {
+                        let token = parseToken(url, index);
+                        console.log("UID : ", uid);
+                        $.ajax({
+                            type: "POST",
+                            url: "/auth/onedrive/sign-in",
+                            data: { token, uid },
+                            dataType: "json",
+                            success: function (response) {
+                                if (response.value === true) {
+                                    console.log("Signed OneDrive user in...")
+                                    //window.location = "/users/interfacePage";
+                                    userLoggedIn();
+                                    loadPage();
+                                }
+                                else {
+                                    console.log("Failed to sign OneDrive user in...");
+                                }
+                            },
+                            error: (err) => {
+
+                            }
+                        });
                     }
-                });
-            }
+                }
+            }).catch((err) => console.log("Error in hasToken(uid) method.", err));
         }
         else console.log("NO USER SIGNED IN.");
     });
@@ -144,6 +159,7 @@ function signoutExpiredUser(uid) {
                     icon: "info"
                 });
                 userLoggedOut();
+                jQuery("#files").html("");
             }
             else {
                 console.log("Error. Did not delete oneDrive user.");
@@ -164,6 +180,8 @@ function signoutUser(uid) {
                 userLoggedOut();
                 swal("Success!", "You have signed out of OneDrive", "success");
                 console.log("Signed OneDrive user out..");
+                jQuery("#files").html("");
+
                 //window.location.replace("/auth/signout");         // Takes you to microsoft sign out but has issues
             }
             else {
@@ -175,40 +193,13 @@ function signoutUser(uid) {
 function userLoggedOut() {
     $("#oneDriveAuth").attr("onclick", "window.location.replace('/auth/signin')");
     $("#oneDriveAuth").text("Sign-In");
+    jQuery("#files").html("");
 }
 function userLoggedIn() {
     $("#oneDriveAuth").attr("onclick", "signoutUser('" + uid + "')");
     $("#oneDriveAuth").text("Sign-Out");
 }
-/**
- * Summary. Manages one drive user status.
- * 
- * Description. Sets OneDrive log-in/log-out button text and changes their links respectively.
- *              logs out user if their token has expired.
- * 
- * @param   {string}    uid   User ID of logged in user
- */
-function checkUserToken(uid) {
-    hasToken(uid).then((result) => {                              // Returns a promise
-        if (result === true) {
-            userLoggedIn();                                 // Sets OD button text and link to log out
 
-            hasTokenExpired(uid).then((result) => {
-                if (result === false) {             //Token was still valid
-                    getToken(uid).then((result) => {
-                        oneDriveUsers = result;
-                        getProfile(oneDriveUsers.token.access_token);
-
-                    });              
-                    
-                }
-            }).catch((err) => console.error("Error in hasTokenExpired(uid) method.", err));
-        }
-        else {
-            userLoggedOut();
-        }
-    }).catch((err) => console.log("Error in hasToken(uid) method.", err));
-}
 function getToken(uid) {
     // Make AJAX call to our server, get access token from database, store it locally for fast access
     return new Promise((resolve, reject) => {
@@ -234,25 +225,40 @@ function getToken(uid) {
 }
 function getProfile(token) {
     // Make call to user, send token and then make microsoft graph request 
-    console.log("Got to get User function")
-    $.ajax({
-        type: "POST",
-        url: "/auth/onedrive/getUser",
-        data: { token: token },
-        dataType: "json",
-        success: function (response) {
-            if (response.value === false) {
-                console.log("Error : Did not get user details : oneDriveAuth.js -> getUser(token).");
-                return null;
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "POST",
+            url: "/auth/onedrive/getUser",
+            data: { token: token },
+            dataType: "json",
+            success: function (response) {
+                if (response.value === false) {
+                    console.log("Error : Did not get user details : oneDriveAuth.js -> getUser(token).");
+                    resolve(null);
+                }
+                else {
+                    resolve(response.profile);
+                }
+            },
+            error: (err) => {
+                console.log("Error in getUser(token)", err);
+                resolve(null);
             }
-            else {
-                oneDriveUsers.profile = response.profile;
-                console.log(oneDriveUsers);
-            }
-        },
-        error: (err) => {
-            console.log("Error in getUser(token)", err);
-            return null;
-        }
+        });
+    });
+}
+function displayProfile() {
+    jQuery("#files").html("<h2>Logged in as: " + oneDriveUsers.profile.displayName || oneDriveUsers.profile.userPrincipalName + "</h2>");
+}
+function loadPage() {
+    getToken(uid).then((result) => {
+        oneDriveUsers = result;
+        getProfile(oneDriveUsers.token.access_token).then((profile) => {
+            oneDriveUsers.profile = profile;
+            console.log(oneDriveUsers);
+            displayProfile();
+            jQuery(".loading").hide();
+
+        });
     });
 }
